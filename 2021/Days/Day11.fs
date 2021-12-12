ad9c95eaad092f3966afdc52f085944e9cd8657f
Johan Åhlgren <johan.ahlgren@griffeye.com>
2021-12-12 15:21:38 +0100

    2021, day 11

--- 2021/Days/Day11.fs
+++ 2021/Days/Day11.fs
@@ -1,7 +1,43 @@
 ﻿namespace AdventOfCode2021
 
+open Lib.DataLoader
+open Lib.Extensions.ListExtensions
+open Lib.Utils
+
 module Day11 =
 
-    let Part1 () = 0
+    let step map =
+        let rec flash flashes m = 
+            let flashing = m |> Array2dUtils.toList |> List.filter (fun (_, _, v) -> v > 9) |> List.map (fun (x, y, _) -> (x, y))
+            let flashCount = flashes + List.length flashing
+            let zeroedMap = Array2D.mapi (fun x y v -> if List.contains (x, y) flashing then 0 else v) m
+            let neighbors = List.map (Array2dUtils.validNeighbors8 zeroedMap) flashing 
+                            |> List.flatten
+                            |> List.filter (fun pos -> not (List.contains pos flashing))
+                            |> List.fold (fun m (x, y) -> MapUtils.incCount (x, y) m) Map.empty
+            let updatedMap = Array2D.mapi (fun x y v -> if v = 0 then 0 else v + MapUtils.getCount (x, y) neighbors) zeroedMap
+            if Map.count neighbors = 0 then (updatedMap, flashCount) else flash flashCount updatedMap
+        Array2D.map ((+) 1) map |> flash 0
 
-    let Part2 () = 0
+    let stepN count map = 
+        let rec stepN' c flashes m =
+            match c with
+            | 0 -> (m, flashes)
+            | n -> let (m', f') = step m
+                   stepN' (c-1) (flashes+f') m'
+        stepN' count 0 map
+
+    let stepUntilAllFlash map = 
+        let rec stepUntilAllFlash' m n =
+            let (m', f') = step m
+            if Array2dUtils.count m' = f' then n
+            else stepUntilAllFlash' m' (n+1)
+        stepUntilAllFlash' map 1
+
+    let Part1 () = 
+        let initialMap = getDataAsArray2D (Spec2021.withPath "Day11Input.txt") 
+                         |> Array2D.map (fun c -> int c - int '0')
+        let (map, flashes) = stepN 100 initialMap
+        flashes
+
+    let Part2 () = getDataAsArray2D (Spec2021.withPath "Day11Input.txt") |> Array2D.map (fun c -> int c - int '0') |> stepUntilAllFlash
---
+++ FSharpLib/Utils/Array2dUtils.fs
@@ -0,0 +1,22 @@
+﻿namespace Lib.Utils
+
+module Array2dUtils =
+
+    let validCoord m (x, y) =
+        let w = Array2D.length1 m
+        let h = Array2D.length2 m
+        0 <= x && x < w && 0 <= y && y < h
+
+    let allNeighbors4 (x, y) = [(x, y-1); (x-1, y); (x+1, y); (x, y+1)]
+
+    let validNeighbors4 m (x, y) = allNeighbors4 (x, y) |> List.filter (validCoord m)
+
+    let allNeighbors8 (x, y) = [(x-1, y-1); (x, y-1); (x+1, y-1); (x-1, y); (x+1, y); (x-1, y+1); (x, y+1); (x+1, y+1)]
+    
+    let validNeighbors8 m (x, y) = allNeighbors8 (x, y) |> List.filter (validCoord m)
+
+    let toListOfValues (m : 'a[,])= m |> Array2D.mapi (fun _ _ c -> c) |> Seq.cast<'a> |> Seq.toList
+
+    let toList (m : 'a[,])= m |> Array2D.mapi (fun x y c -> (x, y, c)) |> Seq.cast<(int * int * 'a)> |> Seq.toList
+
+    let count m = Array2D.length1 m * Array2D.length2 m
