4d31faf42cf0e23c4f854d77b6328bf062e22704
Johan Åhlgren <johan.ahlgren@griffeye.com>
2021-12-12 14:09:58 +0100

    2021, day 12

--- 2021/Days/Day12.fs
+++ 2021/Days/Day12.fs
@@ -1,7 +1,47 @@
 ﻿namespace AdventOfCode2021
 
+open Lib.DataLoader
+open Lib.Extensions.ListExtensions
+open Lib.Regex
+open Lib.Utils.MapUtils
+
 module Day12 =
 
-    let Part1 () = 0
+    let getMap = 
+        let parseLine s = 
+            match s with
+            | Regex @"(.+)-(.+)" [ from; into ] -> (from, into)
+            | _                                 -> failwith "nope"
+        let add m (a, b) = addList a b (addList b a m)
+        getDataAsStringList (Spec2021.withPath "Day12Input.txt") |> List.map parseLine |> List.fold add Map.empty |> Map.add "end" []
 
-    let Part2 () = 0
+    let rec visit visitsMap maxVisitsMap map node =
+        let smallCave (s : string) = s.ToLower() = s
+        if getCount node visitsMap = getCount node maxVisitsMap && smallCave node
+        then [[node]]
+        else let exits = Map.find node map
+             if List.length exits = 0 
+             then [[node]]
+             else let newVisitsMap = incCount node visitsMap
+                  let rest = List.map (visit newVisitsMap maxVisitsMap map) exits |> List.flatten
+                  List.map (fun restPaths -> node :: restPaths) rest
+
+    let getSmallCavesOtherThanStartOrEnd map = 
+        map |> Map.toList |> List.map fst |> List.filter (fun s -> s <> "start" && s <> "end" && s.ToLower() = s)
+
+    let Part1 () = 
+        let map = getMap
+        let smallCaves = getSmallCavesOtherThanStartOrEnd map
+        let maxVisitsMap = smallCaves |> List.map (fun cave -> (cave, 1)) |> Map |> incCount "start"
+        visit Map.empty maxVisitsMap getMap "start" |> List.filter (List.contains "end") |> List.length
+
+    let Part2 () = 
+        let map = getMap
+        let smallCaves = getSmallCavesOtherThanStartOrEnd map
+        let maxVisitsMap caveWithTwoVisits = 
+            smallCaves |> List.map (fun cave -> (cave, if cave = caveWithTwoVisits then 2 else 1)) |> Map |> incCount "start"
+        smallCaves 
+        |> List.map (fun c -> visit Map.empty (maxVisitsMap c) getMap "start" |> List.filter (List.contains "end")) 
+        |> List.flatten 
+        |> Set
+        |> Set.count
---
+++ FSharpLib/Utils/MapUtils.fs
@@ -0,0 +1,19 @@
+﻿namespace Lib.Utils
+
+module MapUtils =
+
+    let incCount key map = match Map.tryFind key map with           
+                           | Some i -> Map.add key (i+1) map
+                           | None   -> Map.add key 1 map
+
+    let getCount key map = match Map.tryFind key map with           
+                           | Some i -> i
+                           | None   -> 0
+
+    let addList key value map = match Map.tryFind key map with           
+                                | Some list -> Map.add key (value :: list) map
+                                | None      -> Map.add key [value] map
+
+    let getList key value map = match Map.tryFind key map with           
+                                | Some list -> list
+                                | None      -> []
