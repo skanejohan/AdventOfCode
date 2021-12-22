12f4cfa71cae6e7faef211911ee326c46b018c15
Johan Åhlgren <johan.ahlgren@griffeye.com>
2021-12-22 17:22:42 +0100

    2021, day 22

--- 2021/Days/Day22.fs
+++ 2021/Days/Day22.fs
@@ -1,7 +1,46 @@
 ﻿namespace AdventOfCode2021
 
-module Day22 =
+open Lib.DataLoader
+open Lib.Regex
+open Lib.Geometry.Box
 
-    let Part1 () = 0
+module Day22 = 
+    let private parseLine line = 
+        match line with
+        | Regex @"on x=([^.]*)..([^,]*),y=([^.]*)..([^,]*),z=([^.]*)..(.*)" [ x1; x2; y1; y2; z1; z2]  -> (true, int x1, int x2, int y1, int y2, int z1, int z2)
+        | Regex @"off x=([^.]*)..([^,]*),y=([^.]*)..([^,]*),z=([^.]*)..(.*)" [ x1; x2; y1; y2; z1; z2] -> (false, int x1, int x2, int y1, int y2, int z1, int z2)
+        | _                                                                    -> failwith "invalid input"
 
-    let Part2 () = 0
+    let rules = getDataAsStringList (Spec2021.withPath "Day22Input.txt") |> List.map parseLine
+
+    let inside x y z (x1, x2, y1, y2, z1, z2) = x1 <= x && x <= x2 && y1 <= y && y <= y2 && z1 <= z && z <= z2
+
+    let evalRule on (x, y, z) (op, x1, x2, y1, y2, z1, z2) = 
+        if inside x y z (x1, x2, y1, y2, z1, z2) 
+        then if op 
+             then Set.add (x, y, z) on
+             else Set.remove (x, y, z) on
+        else on
+
+    let evalRules on pos =
+        let rec ev rules' on' = match rules' with
+                                | r :: rs -> ev rs (evalRule on' pos r)
+                                | _       -> on'
+        ev rules on
+
+    let allCoords = [ for z in -50..50 do for y in -50..50 do for x in -50..50 do (x, y, z) ]
+
+    let Part1 () = List.fold evalRules Set.empty allCoords |> Set.count
+
+    // As expected, the naïve implementation doesn't work for part 2 - use the Box type.
+
+    let Part2 () = 
+        let cubes = rules |> List.map (fun (b, x1, x2, y1, y2, z1, z2) -> (b, (x1, x2, y1, y2, z1, z2)))
+        let rec countLitCubes cubes =
+            match cubes with
+            | (false, _) :: cs  -> countLitCubes cs
+            | (true, box) :: cs -> let remainingBoxes = cs |> List.map (fun (_, b) -> b)
+                                   let overlaps = Box.totalOverlaps box remainingBoxes
+                                   Box.volume box + countLitCubes cs - Box.totalVolume overlaps
+            | _                 -> bigint 0
+        countLitCubes cubes
---
+++ FSharpLib/Geometry/Box.fs
@@ -0,0 +1,46 @@
+﻿namespace Lib.Geometry
+
+module Box =
+
+    // Represents a three-dimensional box in a world where boxes may overlap
+    type Box = Box of int * int * int * int * int * int with
+    
+        // The box representing the overlap of b1 and b2
+        static member overlap b1 b2 =
+            let (minX1, maxX1, minY1, maxY1, minZ1, maxZ1) = b1
+            let (minX2, maxX2, minY2, maxY2, minZ2, maxZ2) = b2
+            let minX = max minX1 minX2
+            let maxX = min maxX1 maxX2
+            let minY = max minY1 minY2
+            let maxY = min maxY1 maxY2
+            let minZ = max minZ1 minZ2
+            let maxZ = min maxZ1 maxZ2
+            if minX > maxX || minY > maxY || minZ > maxZ
+            then None
+            else Some (minX, maxX, minY, maxY, minZ, maxZ)
+    
+        // All boxes representing overlap with the first box
+        static member totalOverlaps box boxes = 
+            let rec tOverlaps box boxes = 
+                match boxes with 
+                | b :: bs -> match Box.overlap box b with
+                             | Some bb -> bb :: tOverlaps box bs
+                             | _       -> tOverlaps box bs
+                | _       -> []
+            tOverlaps box boxes
+
+        // The volume of a box
+        static member volume b =
+            let (minX, maxX, minY, maxY, minZ, maxZ) = b
+            bigint (maxX + 1 - minX) * bigint (maxY + 1 - minY) * bigint (maxZ + 1 - minZ)
+
+        // The total volume of all boxes, taking into account that they may overlap
+        static member totalVolume boxes =
+            let rec tVolume boxes =
+                match boxes with
+                | b :: bs -> let overlaps = Box.totalOverlaps b bs
+                             Box.volume b + tVolume bs - tVolume overlaps
+                | _       -> bigint 0
+            tVolume boxes
+
+
