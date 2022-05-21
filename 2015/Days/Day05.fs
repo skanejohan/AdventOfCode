2d99e58a5030b67034eab7ed6e7d14be68035630
skanejohan <johan_m_ahlgren@hotmail.com>
2022-05-21 17:01:35 +0200

    2015, day 5

---
+++ 2015/Days/Day05.fs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode2015
+
+open Microsoft.FSharp.Collections
+open Library
+
+module Day05 =
+
+    let gt x y = y > x
+
+    let isVowel c = c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u'
+
+    let hasThreeVowels s = String.filter isVowel s |> String.length |> gt 2
+
+    let rec hasLetterTwice s = 
+        match s with
+        | c1 :: c2 :: cs -> if c1 = c2 then true else hasLetterTwice (c2 :: cs)
+        | _              -> false
+        
+    let hasForbiddenString (s : string) = s.Contains "ab" || s.Contains "cd" || s.Contains "pq" || s.Contains "xy"
+
+    let isNice1 s = hasThreeVowels s && hasLetterTwice (Seq.toList s) && not (hasForbiddenString s)
+
+    let Part1 () = GetDataAsStringList "Day05Input.txt" |> List.filter isNice1 |> List.length
+
+    let hasRepeatedPair chars =
+        let rec check cs set last =
+            match cs with
+            | a :: b :: c :: css -> if last <> (a,b) && Set.contains (a,b) set 
+                                    then true
+                                    else check (b :: c :: css) (Set.add (a,b) set) (a,b)
+            | a :: b :: _        -> Set.contains (a,b) set
+            | _                  -> false
+        check chars Set.empty (' ',' ')
+
+    let rec hasSameLetterTwoStepsApart s = 
+        match s with
+        | c1 :: c2 :: c3 :: cs when c1 = c3 -> true
+        | c1 :: cs                          -> hasSameLetterTwoStepsApart cs
+        | _                                 -> false
+
+    let isNice2 s = hasRepeatedPair s && hasSameLetterTwoStepsApart s
+
+    let Part2 () = GetDataAsStringList "Day05Input.txt" |> List.map Seq.toList |> List.filter isNice2 |> List.length
--- Common/Runner2015.cs
+++ Common/Runner2015.cs
@@ -19,8 +19,10 @@ namespace AdventOfCode
                 Verifier.Verify(2639, Day03.Part2(0), "day 3 part 2");
                 Verifier.Verify(282749, Day04.Part1(0), "day 4 part 1");
                 Verifier.Verify(9962624, Day04.Part2(0), "day 4 part 2");
-                Verifier.Verify(569999, Day06.Part1(), "day 6 part 1");
-                Verifier.Verify(17836115, Day06.Part2(), "day 6 part 2");
+                Verifier.Verify(238, Day05.Part1(), "day 5 part 1");
+                Verifier.Verify(69, Day05.Part2 (), "day 5 part 2");
+                Verifier.Verify(569999, Day06.Part1 (), "day 6 part 1");
+                Verifier.Verify(17836115, Day06.Part2 (), "day 6 part 2");
                 Verifier.Verify(3176, Day07.Part1(), "day 7 part 1");
                 Console.WriteLine("All tests for 2015 OK");
             }
