e6595cafb6fe93b4717c4a8f9e34eb71d5b89bd2
Johan Åhlgren <johan.ahlgren@griffeye.com>
2022-12-04 08:40:13 +0100

    2022, day 04

--- 2022/Day04/Solver.cs
+++ 2022/Day04/Solver.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpLib;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Y2022.Day04
 {
@@ -7,17 +8,31 @@ namespace Y2022.Day04
     {
         public static long Part1()
         {
-            return 0;
+            return LoadData("data.txt").Count(s => s.Item1.IsSubsetOf(s.Item2) || s.Item2.IsSubsetOf(s.Item1));
         }
 
         public static long Part2()
         {
-            return 0;
+            return LoadData("data.txt").Count(s => s.Item1.Intersect(s.Item2).Any());
         }
 
-        private static IEnumerable<string> LoadData(string fileName)
+        private static IEnumerable<(HashSet<int>, HashSet<int>)> LoadData(string fileName)
         {
-            return new DataLoader(2022, 4).ReadStrings(fileName);
+            return new DataLoader(2022, 4).ReadStrings(fileName).Select(ParseLine);
+
+            (HashSet<int>, HashSet<int>) ParseLine(string line)
+            {
+                var split = line.Split(',');
+                var first = split[0].Split('-');
+                var second = split[1].Split('-');
+                var a = int.Parse(first[0]);
+                var b = int.Parse(first[1]);
+                var c = int.Parse(second[0]);
+                var d = int.Parse(second[1]);
+                return (
+                    new HashSet<int>(Enumerable.Range(a, b - a + 1)),
+                    new HashSet<int>(Enumerable.Range(c, d - c + 1)));
+            }
         }
     }
 }
