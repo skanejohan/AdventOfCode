2d55025960808cb48f72d675966145f1c8d5140f
Johan Åhlgren <johan.ahlgren@griffeye.com>
2022-12-06 07:01:50 +0100

    2022, day 06

--- 2022/Day06/Solver.cs
+++ 2022/Day06/Solver.cs
@@ -1,5 +1,8 @@
 ﻿using CSharpLib;
+using CSharpLib.DataStructures;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Y2022.Day06
 {
@@ -7,17 +10,35 @@ namespace Y2022.Day06
     {
         public static long Part1()
         {
-            return 0;
+            return Find(LoadData("data.txt"), 4);
         }
 
         public static long Part2()
         {
+            return Find(LoadData("data.txt"), 14);
+        }
+
+        public static long Find(string s, int count)
+        {
+            var cs = new CountedSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i >= count)
+                {
+                    cs.Remove(s[i - count]);
+                }
+                cs.Add(s[i]);
+                if (cs.Count() == count)
+                {
+                    return i + 1;
+                }
+            }
             return 0;
         }
 
-        private static IEnumerable<string> LoadData(string fileName)
+        private static string LoadData(string fileName)
         {
-            return new DataLoader(2022, 6).ReadStrings(fileName);
+            return new DataLoader(2022, 6).ReadStrings(fileName).First();
         }
     }
 }
--- Common/Runner2022.cs
+++ Common/Runner2022.cs
@@ -20,8 +20,8 @@ namespace AdventOfCode
                 Verifier.Verify(917, Y2022.Day04.Solver.Part2(), "day 4 part 2");
                 Verifier.Verify("WSFTMRHPP", Y2022.Day05.Solver.Part1(), "day 5 part 1");
                 Verifier.Verify("GSLCMFBRP", Y2022.Day05.Solver.Part2(), "day 5 part 2");
-                Verifier.Verify(0, Y2022.Day06.Solver.Part1(), "day 6 part 1");
-                Verifier.Verify(0, Y2022.Day06.Solver.Part2(), "day 6 part 2");
+                Verifier.Verify(1987, Y2022.Day06.Solver.Part1(), "day 6 part 1");
+                Verifier.Verify(3059, Y2022.Day06.Solver.Part2(), "day 6 part 2");
                 Verifier.Verify(0, Y2022.Day07.Solver.Part1(), "day 7 part 1");
                 Verifier.Verify(0, Y2022.Day07.Solver.Part2(), "day 7 part 2");
                 Verifier.Verify(0, Y2022.Day08.Solver.Part1(), "day 8 part 1");
