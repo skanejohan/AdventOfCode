6a999133ac99a63e3351c23930577872de67e22d
skanejohan <johan_m_ahlgren@hotmail.com>
2024-11-17 17:27:06 +0100

    2018, day 18 part 1

---
+++ 2019/Days/Day18.cs
@@ -0,0 +1,127 @@
+﻿using AdventOfCode.Common;
+using CSharpLib.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days2019
+{
+    public static class Day18
+    {
+        public static long Part1()
+        {
+            LoadData();
+            var initialState = new State(doors, keys, pos);
+            var result = Dijkstra<State>.Solve(initialState, CalculatePossibleStates, s => s.Keys.Count == 0, PrettyPrint);
+            return result.TotalCost;
+        }
+
+        public static long Part2()
+        {
+            return 0;
+        }
+
+        private static IEnumerable<Edge<State>> CalculatePossibleStates(State state)
+        {
+            if (TryGetPossibleState(-1, 0, out var s)) { yield return new(state, s, 1); }
+            if (TryGetPossibleState(0, -1, out s)) { yield return new(state, s, 1); }
+            if (TryGetPossibleState(0, 1, out s)) { yield return new(state, s, 1); }
+            if (TryGetPossibleState(1, 0, out s)) { yield return new(state, s, 1); }
+
+            bool TryGetPossibleState(int rowOffset, int colOffset, out State newState)
+            {
+                var row = state.Pos.Row + rowOffset;
+                var col = state.Pos.Col + colOffset;
+                var isDoor = state.Doors.TryGetValue((row, col), out _);
+                if (row < 0 || row > rows-1 || col < 0 || col > cols-1 || isDoor || walls.Contains((row, col)))
+                {
+                    newState = state;
+                    return false;
+                }
+
+                var newKeys = new Dictionary<(int Row, int Col), char>(state.Keys);
+                var newDoors = new Dictionary<(int Row, int Col), char>(state.Doors);
+                var isKey = newKeys.TryGetValue((row, col), out var key);
+                if (isKey)
+                {
+                    newKeys.Remove((row, col));
+                    var door = newDoors.FirstOrDefault(kvp => kvp.Value == char.ToUpper(key));
+                    if (door.Value == char.ToUpper(key))
+                    {
+                        newDoors.Remove(door.Key);
+                    }
+                }
+
+                newState = new(newDoors, newKeys, (row, col));
+                return true;
+            }
+        }
+
+        private static void LoadData()
+        {
+            var row = 0;
+            foreach (var line in DataReader.ReadStrings("Day18Input.txt").ToList())
+            {
+                cols = line.Length;
+                var col = 0;
+                foreach (var c in line.ToCharArray())
+                {
+                    if (c == '@')
+                    {
+                        pos = (row, col);
+                    }
+                    else if (c == '#')
+                    {
+                        walls.Add((row, col));
+                    }
+                    else if (char.IsUpper(c))
+                    {
+                        doors[(row, col)] = c;
+                    }
+                    else if (char.IsLower(c))
+                    {
+                        keys[(row, col)] = c;
+                    }
+                    col++;
+                }
+                row++;
+            }
+            rows = row;
+        }
+
+        private static string PrettyPrint(State state)
+        {
+            var doorsInOrder = state.Doors.Select(kvp => $"({kvp.Key.Row},{kvp.Key.Col})={kvp.Value}").ToList();
+            doorsInOrder.Sort();
+
+            var keysInOrder = state.Keys.Select(kvp => $"({kvp.Key.Row},{kvp.Key.Col})={kvp.Value}").ToList();
+            keysInOrder.Sort();
+
+            var keys = '[' + string.Join(',', keysInOrder) + ']';
+            var doors = '[' + string.Join(',', doorsInOrder) + ']';
+            
+            return $"{state.Pos} {keys} {doors}";
+        }
+
+        private static void PrintResult(State initialState, Solution<State> result)
+        {
+            Console.WriteLine(PrettyPrint(initialState));
+            foreach (var s in result.Path)
+            {
+                Console.WriteLine(PrettyPrint(s.Destination));
+            }
+        }
+
+        // Fixed, will not change
+        private static HashSet<(int, int)> walls = [];
+        private static int cols;
+        private static int rows;
+
+        // Will change, must be part of state
+        private static Dictionary<(int Row, int Col), char> doors = [];
+        private static Dictionary<(int Row, int Col), char> keys = [];
+        private static (int Row, int Col) pos;
+
+        record State(Dictionary<(int Row, int Col), char> Doors, Dictionary<(int Row, int Col), char> Keys, (int Row, int Col) Pos);
+    }
+}
--- CSharpLib/Algorithms/Dijkstra.cs
+++ CSharpLib/Algorithms/Dijkstra.cs
@@ -2,79 +2,84 @@
 using System;
 using System.Collections.Generic;
 
-namespace CSharpLib.Algorithms
+namespace CSharpLib.Algorithms;
+
+public static class Dijkstra<T> where T : notnull, IEquatable<T>
 {
-    public class Dijkstra<T> where T : notnull, IEquatable<T>
+    /// <summary>
+    /// Solves a shortest-path graph problem using Dijkstra's algorithm.
+    /// </summary>
+    /// <param name="start">This is the start (initial) state.</param>
+    /// <param name="calculateEdgesToNeighbors">Given a state, it is this function's job to calculate the edges to all adjacent states.</param>
+    /// <param name="isTarget">Given a state, this function should tell if the state is the target state or not.</param>
+    /// <param name="getIdentifier">If left out, the state itself will be placed in a hash set indicating which states that have been seen.
+    /// You may specify a function that returns an object (e.g. a string) uniquely representing a state. This may be needed if the standard
+    /// equality comparison does not work (and we fail to detect states that we have already seen).</param>
+    /// <returns>An object indicating all edges, and the total cost.</returns>
+    /// <exception cref="Exception">Thrown if no path was found.</exception>
+    public static Solution<T> Solve(T start, Func<T, IEnumerable<Edge<T>>> calculateEdgesToNeighbors, Func<T, bool> isTarget, Func<T, object>? getIdentifier = null)
     {
-        public Dijkstra(Func<T, bool> targetIsReached = null)
-        {
-            this.targetIsReached = targetIsReached;
-        }
+        Func<T, object> id = getIdentifier ?? (t => t);
 
-        public IEnumerable<(T T, long Distance)> ShortestPath()
+        var visitedNodes = new HashSet<object>();
+        var nodesToVisit = new SimplePriorityQueue<T, long>();
+        var lowestCostToDestinationNode = new Dictionary<T, long>() 
+        { 
+            [start] = 0 
+        };
+        var lowestCostEdgeToDestinationNode = new Dictionary<T, Edge<T>>
         {
-            var cell = target;
-            var items = new List<(T, long)>
-            {
-                (target, cost)
-            };
-            do
-            {
-                items.Add(edges[cell]);
-                cell = edges[cell].Item1;
-            } while (!cell.Equals(start));
-            items.Reverse();
-            return items;
-        }
+            [start] = new(start, start, 0)
+        };
 
-        public long Solve(T start, T target, Func<T, IEnumerable<(T, long)>> transformer)
+        nodesToVisit.Enqueue(start, 0);
+        while (nodesToVisit.Count > 0)
         {
-            if (targetIsReached == null)
+            var currentNode = nodesToVisit.Dequeue();
+
+            if (visitedNodes.Contains(id(currentNode)))
             {
-                targetIsReached = c => c.Equals(target);
+                continue;
             }
-            edges.Clear();
-            edges[start] = (start, 0);
-            var visited = new HashSet<T>();
-            var queue = new SimplePriorityQueue<T, long>();
-            var dist = new Dictionary<T, long>() { [start] = 0 };
-            this.target = target;
-            this.start = start;
 
-            queue.Enqueue(start, 0);
-            while (queue.Count > 0)
+            var currentCost = lowestCostToDestinationNode[currentNode];
+
+            visitedNodes.Add(id(currentNode));
+
+            if (isTarget(currentNode))
             {
-                var current = queue.Dequeue();
-                if (!visited.Contains(current))
+
+                return new(GetPath(start, currentNode, lowestCostEdgeToDestinationNode), currentCost);
+            }
+
+            foreach (var edge in calculateEdgesToNeighbors(currentNode))
+            {
+                var totalCost = currentCost + edge.Cost;
+                if (totalCost < lowestCostToDestinationNode.GetValueOrDefault(edge.Destination, long.MaxValue))
                 {
-                    var currentCost = dist[current];
-
-                    visited.Add(current);
-                    if (targetIsReached(current))
-                    {
-                        return currentCost;
-                    }
-
-                    foreach (var (next, cost) in transformer(current))
-                    {
-                        var totalCost = currentCost + cost;
-                        if (totalCost < dist.GetValueOrDefault(next, long.MaxValue))
-                        {
-                            dist[next] = totalCost;
-                            edges[next] = (current, currentCost);
-                            queue.Enqueue(next, totalCost);
-                        }
-                    }
+                    lowestCostToDestinationNode[edge.Destination] = totalCost;
+                    lowestCostEdgeToDestinationNode[edge.Destination] = new(currentNode, edge.Destination, currentCost);
+                    nodesToVisit.Enqueue(edge.Destination, totalCost);
                 }
             }
-
-            cost = dist[target];
-            return cost;
         }
 
-        private readonly Dictionary<T, (T, long)> edges = new Dictionary<T, (T, long)>();
-        private Func<T, bool> targetIsReached;
-        private T start, target;
-        private long cost;
+        throw new Exception("No path found");
+    }
+
+    private static List<Edge<T>> GetPath(T source, T destination, Dictionary<T, Edge<T>> lowestCostEdgeToDestination)
+    {
+        var cell = destination;
+        var items = new List<Edge<T>>();
+        do
+        {
+            items.Add(lowestCostEdgeToDestination[cell]);
+            cell = lowestCostEdgeToDestination[cell].Source;
+        } while (!cell.Equals(source));
+        items.Reverse();
+        return items;
     }
 }
+
+public record Edge<T>(T Source, T Destination, long Cost);
+public record Solution<T>(List<Edge<T>> Path, long TotalCost);
