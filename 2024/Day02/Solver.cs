f2946f8aedf18b392878f9f7a0181027314a752b
skanejohan <johan_m_ahlgren@hotmail.com>
2024-12-02 07:38:36 +0100

    2024, day 2

--- 2024/Day02/Solver.cs
+++ 2024/Day02/Solver.cs
@@ -1,14 +1,82 @@
-﻿namespace Y2024.Day02;
+﻿using CSharpLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Y2024.Day02;
 
 public static class Solver
 {
     public static long Part1()
     {
-        return 0;
+        return Solve("Data.txt", LineOk); 
     }
 
     public static long Part2()
     {
-        return 0;
+        return Solve("Data.txt", AnyLineOk);
+
+        static bool AnyLineOk(List<int> line)
+        {
+            if (LineOk(line))
+            {
+                return true;
+            }
+
+            for (var i = 0; i < line.Count; i++)
+            {
+                var l = new List<int>(line);
+                l.RemoveAt(i);
+                if (LineOk(l))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private static long Solve(string fileName, Func<List<int>, bool> lineOk)
+    {
+        var n = 0;
+        foreach (var line in LoadData(fileName))
+        {
+            if (lineOk(line))
+            {
+                n++;
+            }
+        }
+        return n;
+    }
+
+    private static bool LineOk(List<int> line)
+    {
+        var ok = true;
+        Func<int, int, bool> fn = line[1] > line[0] ? IncreasingOk : DecreasingOk;
+
+        for (int i = 1; i < line.Count; i++)
+        {
+            if (!fn(line[i - 1], line[i]))
+            {
+                ok = false;
+            }
+        }
+        return ok;
+    }
+
+    private static IEnumerable<List<int>> LoadData(string fileName)
+    {
+        return new DataLoader(2024, 2).ReadStrings(fileName).Select(s => s.Split(' ').Select(int.Parse).ToList());
+    }
+
+    private static bool IncreasingOk(int a, int b)
+    {
+        return a < b && Math.Abs(a - b) < 4;
+    }
+
+    private static bool DecreasingOk(int a, int b)
+    {
+        return a > b && Math.Abs(a - b) < 4;
     }
 }
--- Common/Runner2024.cs
+++ Common/Runner2024.cs
@@ -12,8 +12,8 @@ public static class Runner2024
             Console.WriteLine("Running tests for 2024");
             //Verifier.Verify(936063, Y2024.Day01.Solver.Part1(), "day 01 part 1");
             //Verifier.Verify(23150395, Y2024.Day01.Solver.Part2(), "day 01 part 2");
-            Verifier.Verify(0, Y2024.Day02.Solver.Part1(), "day 02 part 1");
-            Verifier.Verify(0, Y2024.Day02.Solver.Part2(), "day 02 part 2");
+            //Verifier.Verify(379, Y2024.Day02.Solver.Part1(), "day 02 part 1");
+            //Verifier.Verify(430, Y2024.Day02.Solver.Part2(), "day 02 part 2");
             Verifier.Verify(0, Y2024.Day03.Solver.Part1(), "day 03 part 1");
             Verifier.Verify(0, Y2024.Day03.Solver.Part2(), "day 03 part 2");
             Verifier.Verify(0, Y2024.Day04.Solver.Part1(), "day 04 part 1");
