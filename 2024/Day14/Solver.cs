6c4aaea044d633d05761b1c6693ee91a78b3e901
skanejohan <johan_m_ahlgren@hotmail.com>
2024-12-14 09:30:27 +0100

    2024, day 14

--- 2024/Day14/Solver.cs
+++ 2024/Day14/Solver.cs
@@ -1,14 +1,109 @@
-﻿namespace Y2024.Day14;
+﻿using CSharpLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Y2024.Day14;
 
 public static class Solver
 {
     public static long Part1()
     {
-        return 0;
+        var width = 101;
+        var height = 103;
+        var steps = 100;
+        var robots = LoadData("data.txt");
+        MoveRobots(robots, width, height, steps);
+        var q1 = robots.Where(r => r.X < width / 2 && r.Y < height / 2).Count();
+        var q2 = robots.Where(r => r.X > width / 2 && r.Y < height / 2).Count();
+        var q3 = robots.Where(r => r.X < width / 2 && r.Y > height / 2).Count();
+        var q4 = robots.Where(r => r.X > width / 2 && r.Y > height / 2).Count();
+        return q1 * q2 * q3 * q4;
     }
 
     public static long Part2()
     {
-        return 0;
+        // Looping through steps, I saw that after 6516 steps, there was a clear Christmas tree.
+        // I manually figured out its positions, then checked the first step where many of them
+        // were available. Turns out that was 6516...
+
+        var n = 0;
+        var width = 101;
+        var height = 103;
+        var robots = LoadData("data.txt");
+
+        while (true)
+        {
+            var hs = robots.Select(r => (r.X, r.Y)).ToHashSet();
+            var hasTopRow = true;
+            for (var x = 41; x < 72; x++)
+            {
+                if (!hs.Contains((x, 41)))
+                {
+                    hasTopRow = false;
+                    break;
+                }
+            }
+            var hasLeftCol = true;
+            for (var y = 41; y < 74; y++)
+            {
+                if (!hs.Contains((41, y)))
+                {
+                    hasLeftCol = false;
+                    break;
+                }
+            }
+
+            if (hasTopRow && hasLeftCol)
+            {
+                Console.WriteLine(n);
+                foreach (var s in Print(robots, width, height))
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                break;
+            }
+            MoveRobots(robots, width, height, 1);
+            n++;
+        }
+
+        return 6516;
+    }
+
+    static List<(long X, long Y, long DX, long DY)> LoadData(string fileName)
+    {
+        return new DataLoader(2024, 14).ReadStrings(fileName).Select(s => 
+        {
+            var r = s.Split(",").Select(long.Parse).ToList();
+            return (r[0], r[1], r[2], r[3]);
+        }).ToList();
+    }
+
+    static void MoveRobots(List<(long X, long Y, long DX, long DY)> robots, int width, int height, int steps)
+    {
+        for (var i = 0; i < robots.Count; i++)
+        {
+            var (X, Y, DX, DY) = robots[i];
+            var x = (width + X + steps * DX) % width;
+            var y = (height + Y + steps * DY) % height;
+            if (x < 0) x += width;
+            if (y < 0) y += height;
+            robots[i] = (x, y, DX, DY);
+        }
+    }
+
+    static IEnumerable<string> Print(List<(long X, long Y, long DX, long DY)> robots, int width, int height)
+    {
+        for (var y = 0; y < height; y++)
+        {
+            var s = "";
+            for (var x = 0; x < width; x++)
+            {
+                var n = robots.Count(r => r.X == x && r.Y == y);
+                s += n == 0 ? "." : $"{n}";
+            }
+            yield return s;
+        }
     }
 }
--- Common/Runner2024.cs
+++ Common/Runner2024.cs
@@ -34,10 +34,10 @@ public static class Runner2024
             //Verifier.Verify(218279375708592, Y2024.Day11.Solver.Part2(), "day 11 part 2");
             //Verifier.Verify(1450816, Y2024.Day12.Solver.Part1(), "day 12 part 1");
             //Verifier.Verify(865662, Y2024.Day12.Solver.Part2(), "day 12 part 2");
-            Verifier.Verify(35255, Y2024.Day13.Solver.Part1(), "day 13 part 1");
+            //Verifier.Verify(35255, Y2024.Day13.Solver.Part1(), "day 13 part 1");
             Verifier.Verify(0, Y2024.Day13.Solver.Part2(), "day 13 part 2");
-            Verifier.Verify(0, Y2024.Day14.Solver.Part1(), "day 14 part 1");
-            Verifier.Verify(0, Y2024.Day14.Solver.Part2(), "day 14 part 2");
+            //Verifier.Verify(217132650, Y2024.Day14.Solver.Part1(), "day 14 part 1");
+            //Verifier.Verify(6516, Y2024.Day14.Solver.Part2(), "day 14 part 2");
             Verifier.Verify(0, Y2024.Day15.Solver.Part1(), "day 15 part 1");
             Verifier.Verify(0, Y2024.Day15.Solver.Part2(), "day 15 part 2");
             Verifier.Verify(0, Y2024.Day16.Solver.Part1(), "day 16 part 1");
