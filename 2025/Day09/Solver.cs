e230b97cf8593f54fb3e1817c5dd9e8de17f8704
skanejohan <johan_m_ahlgren@hotmail.com>
2025-12-13 14:37:21 +0100

    2025, day 9 pt 2, cleaned up but still wrong

--- 2025/Day09/Solver.cs
+++ 2025/Day09/Solver.cs
@@ -11,93 +11,167 @@ public static class Solver
     {
         var redTiles = LoadRedTiles("Data.txt");
         var rectangles = GetRectangles(redTiles);
-
-        long area = 0;
-        foreach(var (X1, Y1, X2, Y2, Area) in rectangles)
-        {
-            area = Math.Max(area, Area);
-        }
-        return area;
+        return rectangles.Select(r => r.Area()).Max();
     }
 
-    public static long Part2() // 4743386600 is too high
+    public static long Part2() // 4743386600 is too high, 4638696212 is too high, 2125643728 is too high, 2019428457 is wrong
     {
         var redTiles = LoadRedTiles("Data.txt");
-        var (horizontalEdges, verticalEdges) = GetEdges(redTiles);
-        var rectangles = GetRectangles(redTiles).Where(r => InsidePolygon((r.X1, r.Y1, r.X2, r.Y2), horizontalEdges, verticalEdges));
-
-        long area = 0;
-        foreach (var (X1, Y1, X2, Y2, Area) in rectangles)
-        {
-            area = Math.Max(area, Area);
-        }
-        //return area;
-        return 0;
+        var rectangles = GetRectangles(redTiles);
+        var (horizontal, vertical) = GetEdges([.. redTiles, redTiles[0]]);
+        return rectangles.Where(r => IsValidRectangle(r, horizontal, vertical)).Select(r => r.Area()).Max();
     }
 
-    private static List<(int X, int Y)> LoadRedTiles(string fileName)
+    private static List<Point> LoadRedTiles(string fileName)
     {
-        return new DataLoader(2025, 9)
-            .ReadStrings(fileName)
-            .Select(s => s.Split(','))
-            .Select(s => { var l = s.Select(int.Parse).ToList(); return (l[0], l[1]); })
-            .ToList();
+        return [.. new DataLoader(2025, 9).ReadStrings(fileName).Select(s => s.Split(',')).Select(s => { var l = s.Select(int.Parse).ToList(); return new Point(l[0], l[1]); })];
     }
 
-    private static IEnumerable<(int X1, int Y1, int X2, int Y2, long Area)> GetRectangles(List<(int X, int Y)> redTiles)
+    private static IEnumerable<Rectangle> GetRectangles(List<Point> redTiles)
     {
         for (var i = 0; i < redTiles.Count - 1; i++)
         {
             for (var j = i + 1; j < redTiles.Count; j++)
             {
-                var (X1, Y1) = redTiles[i];
-                var (X2, Y2) = redTiles[j];
-                long dx = Math.Abs(X2 - X1) + 1;
-                long dy = Math.Abs(Y2 - Y1) + 1;
-                yield return (X1, Y1, X2, Y2, dx * dy);
+                yield return new Rectangle(redTiles[i].X, redTiles[i].Y, redTiles[j].X, redTiles[j].Y);
             }
         }
     }
 
-    private static (List<(int X1, int X2, int Y)> Horizontal, List<(int X, int Y1, int Y2)> Vertical) GetEdges(List<(int X, int Y)> redTiles)
+    private static (List<HLine> Horizontal, List<VLine> Vertical) GetEdges(List<Point> polygon)
     {
-        var allPoints = redTiles.Append(redTiles[0]).ToList();
-        var edges = new List<(int X1, int Y1, int X2, int Y2)>();
-        for (var i = 0; i < allPoints.Count - 1; i++)
+        List<VLine> vertical = [];
+        List<HLine> horizontal = [];
+        for (var i = 0; i < polygon.Count - 1; i++)
         {
-            var p1 = allPoints[i];
-            var p2 = allPoints[i + 1];
-            edges.Add((p1.X, p1.Y, p2.X, p2.Y));
+            var p1 = polygon[i];
+            var p2 = polygon[i + 1];
+            if (p1.X == p2.X)
+            {
+                vertical.Add(new VLine(p1.X, p1.Y, p2.Y));
+            }
+            else
+            {
+                horizontal.Add(new HLine(p1.X, p2.X, p1.Y));
+            }
         }
-        var horizontal = edges.Where(e => e.Y1 == e.Y2).Select(e => e.X1 < e.X2 ? (e.X1, e.X2, e.Y1) : (e.X2, e.X1, e.Y1)).ToList();
-        var vertical = edges.Where(e => e.X1 == e.X2).Select(e => e.Y1 < e.Y2 ? (e.X1, e.Y1, e.Y2) : (e.X1, e.Y2, e.Y1)).ToList();
         return (horizontal, vertical);
     }
 
-    private static bool InsidePolygon((int X1, int Y1, int X2, int Y2) rect, List<(int X1, int X2, int Y)> horizontalEdges, List<(int X, int Y1, int Y2)> verticalEdges)
+    private static bool IsValidRectangle(Rectangle r, List<HLine> horizontalEdges, List<VLine> verticalEdges)
     {
-        var topLeftInside = InsidePolygon((rect.X1, rect.Y1), horizontalEdges, verticalEdges);
-        var topRightInside = InsidePolygon((rect.X2, rect.Y1), horizontalEdges, verticalEdges);
-        var bottomRightInside = InsidePolygon((rect.X2, rect.Y2), horizontalEdges, verticalEdges);
-        var bottomLeftInside = InsidePolygon((rect.X1, rect.Y2), horizontalEdges, verticalEdges);
-
-        return topLeftInside && topRightInside && bottomRightInside && bottomLeftInside;
-    }
-
-    private static bool InsidePolygon((int X, int Y) pos, List<(int X1, int X2, int Y)> horizontalEdges, List<(int X, int Y1, int Y2)> verticalEdges)
-    {
-        // Does the point lie on a horizontal line?
-        foreach (var (X1, X2, Y) in horizontalEdges.Where(e => e.Y == pos.Y))
+        // Check that all four corners are inside the polygon, or return false.
+        foreach(var p in r.Corners())
         {
-            if (X1 <= pos.X && X2 >= pos.X)
+            if (!IsInPolygon(p, horizontalEdges, verticalEdges))
             {
-                return true;
+                return false;
             }
         }
 
-        // How many vertical lines (that will get crossed if moving horizontally) are further to the right than the point? Odd => Inside, even => not inside.
-        var crossableVerticalEdges = verticalEdges.Where(e => e.Y1 <= pos.Y && e.Y2 >= pos.Y);
-        var crossedVerticalEdges = crossableVerticalEdges.Where(e => e.X > pos.X);
-        return crossedVerticalEdges.Count() % 2 != 0;
+        // If the rectangle's left or right edge crosses any horizontal edge in the polygon, return false.
+        foreach (var hEdge in horizontalEdges)
+        {
+            foreach(var vEdge in r.VEdges())
+            {
+                if (LinesCross(vEdge, hEdge))
+                {
+                    return false;
+                }
+            }
+        }
+
+        // If the rectangle's top or bottom edge crosses any vertical edge in the polygon, return false.
+        foreach (var vEdge in verticalEdges)
+        {
+            foreach (var hEdge in r.HEdges())
+            {
+                if (LinesCross(vEdge, hEdge))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsInPolygon(Point p, List<HLine> horizontalEdges, List<VLine> verticalEdges)
+    {
+        if (horizontalEdges.Any(e => e.ContainsPoint(p)))
+        {
+            return true;
+        }
+
+        if (verticalEdges.Any(e => e.ContainsPoint(p)))
+        {
+            return true;
+        }
+
+        // How many vertical edges would we cross if we move to the right from this point? It if is an odd number, we are inside the polygon.
+        var edgesToTheRight = verticalEdges.Where(e => e.X > p.X && e.MinY <= p.Y && e.MaxY >= p.Y).Count();
+        return edgesToTheRight % 2 == 1;
+    }
+
+    private static bool LinesCross(VLine vLine, HLine hLine)
+    {
+        return hLine.MinX < vLine.X && hLine.MaxX > vLine.X && vLine.MinY < hLine.Y && vLine.MaxY > hLine.Y;
+    }
+
+    class Point(int x, int y)
+    {
+        public int X { get; } = x;
+        public int Y { get; } = y;
+    }
+
+    class Rectangle(int x1, int y1, int x2, int y2)
+    {
+        public int X1 { get; } = x1;
+        public int Y1 { get; } = y1;
+        public int X2 { get; } = x2;
+        public int Y2 { get; } = y2;
+        public long Area()
+        {
+            long dx = Math.Abs(X2 - X1) + 1;
+            long dy = Math.Abs(Y2 - Y1) + 1;
+            return dx * dy;
+        }
+        public IEnumerable<Point> Corners()
+        {
+            yield return new Point(X1, Y1);
+            yield return new Point(X2, Y1);
+            yield return new Point(X2, Y2);
+            yield return new Point(X1, Y2);
+        }
+        public IEnumerable<HLine> HEdges()
+        {
+            yield return new HLine(X1, X2, Y1);
+            yield return new HLine(X1, X2, Y2);
+        }
+        public IEnumerable<VLine> VEdges()
+        {
+            yield return new VLine(X1, Y1, Y2);
+            yield return new VLine(X2, Y1, Y2);
+        }
+    }
+
+    class HLine(int x1, int x2, int y)
+    {
+        public int X1 { get; } = x1;
+        public int X2 { get; } = x2;
+        public int Y { get; } = y;
+        public int MinX => Math.Min(X1, X2);
+        public int MaxX => Math.Max(X1, X2);
+        public bool ContainsPoint(Point p) => p.Y == Y && p.X >= MinX && p.X <= MaxX;
+    }
+
+    class VLine(int x, int y1, int y2)
+    {
+        public int X { get; } = x;
+        public int Y1 { get; } = y1;
+        public int Y2 { get; } = y2;
+        public int MinY => Math.Min(Y1, Y2);
+        public int MaxY => Math.Max(Y1, Y2);
+        public bool ContainsPoint(Point p) => p.X == X && p.Y >= MinY && p.Y <= MaxY;
     }
 }
